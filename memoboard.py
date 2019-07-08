1d2d5c891eb5fe4c75b85dddf1cf1fae033a19ed
jaeho3918 <46740030+babbybear@users.noreply.github.com>
2019-07-08 19:23:06 +0900

    1

--- memoboard.py
+++ memoboard.py
@@ -18,11 +18,9 @@ class MainWindow(QMainWindow):
         self.setWindowTitle('Memoboard')
         message = "{0}     {1}".format("Ready", self.time.toString(Qt.DefaultLocaleLongDate))
         self.statusBar().showMessage(message)
-        self.resize(1000,927)
+        self.resize(774,879)
         self.center()
-
         self.status()
-
         self.show()
 
     def center(self):
@@ -33,17 +31,69 @@ class MainWindow(QMainWindow):
 
     def status(self):
         self.centralWidget = QWidget(self)
+
+        status = ["대사관","아포스티유","공증","등기","견적서","계산서"]
+        status_stylesheet = {"대사관":"color: black;"              #빨간색
+                                "background-color: #7EB9FF;"
+                                "border-style: solid;"
+                                "border-width: 3px;"
+                                "border-color: #7EB9FF;"
+                                "border-radius: 8px",
+                        "아포스티유":"color: black;"           #주황색
+                                "background-color: #ff917b;"
+                                "border-style: solid;"
+                                "border-width: 3px;"
+                                "border-color: #ff917b;"
+                                "border-radius: 8px",
+                        "공증": "color: black;"               #초록색
+                                "background-color: #b3eb2b;"
+                                "border-style: solid;"
+                                "border-width: 3px;"
+                                "border-color: #b3eb2b;"
+                                "border-radius: 8px",
+                        "등기": "color: black;"               #보라색
+                                "background-color: #C486FF;"
+                                "border-style: solid;"
+                                "border-width: 3px;"
+                                "border-color: #C486FF;"
+                                "border-radius: 8px",
+                        "견적서": "color: black;"             #노란색
+                                "background-color: #FEFF7F;"
+                                "border-style: solid;"
+                                "border-width: 3px;"
+                                "border-color: #FEFF7F;"
+                                "border-radius: 8px",
+                        "계산서": "color: black;"             #노란색
+                                "background-color: #FEFF7F;"
+                                "border-style: solid;"
+                                "border-width: 3px;"
+                                "border-color: #FEFF7F;"
+                                "border-radius: 8px"
+                        }
+
         labels = [QLabel(self.centralWidget) for i in range(6)]
         for idx,item in enumerate(labels):
-            item.setStyleSheet("color: black;"
-                              "border-style: solid;"
-                              "border-width: 2px;"
-                              "border-color: #FA8072;"
-                              "border-radius: 3px")
-            item.setText("뀨:{}".format(idx))
+            font = QFont()
+            font.setFamily("맑은 고딕")
+            font.setPointSize(18)
+            font.setBold(True)
 
-        hbox = QHBoxLayout(self.centralWidget)
-        hboxaddwidget = [hbox.addWidget(i) for i in labels]
+            item.setFont(font)
+            item.setStyleSheet(status_stylesheet[status[idx]])
+            item.setText("{}".format(status[idx]))
+            item.setAlignment(Qt.AlignCenter)
+            print(item.x())
+
+        print(status_stylesheet["계산서"])
+
+        self.hbox = QHBoxLayout(self.centralWidget)
+
+        wide = 740 / len(labels)
+
+        for idx, item in enumerate(labels):
+            item.resize(wide,30)
+
+
 
         self.setCentralWidget(self.centralWidget)
 
