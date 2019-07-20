bb5e822e0b9dce7729c5d7f7dfa351e7e71c8a6d
jaeho3918 <46740030+babbybear@users.noreply.github.com>
2019-07-21 00:31:10 +0900

    1

--- query.py
+++ query.py
@@ -1,5 +1,8 @@
 import pymysql
 
-conn = pymysql.connect(host='112.172.237.233:8505', user='comm', password='gana2939!', db='gana', charset='utf8mb4')
+conn = pymysql.connect(host='112.172.237.233', user='comm', password='gana2939!', db='gana', charset='utf8mb4')
 
 curs = conn.cursor()
+
+print(curs)
+print('111111111sudo ')
