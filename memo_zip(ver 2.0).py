7094d2baac6a87816374a76432204c52333c8d11
jaeho3918 <46740030+babbybear@users.noreply.github.com>
2019-08-18 17:35:52 +0900

    1

--- memo_zip(ver 2.0).py
+++ memo_zip(ver 2.0).py
@@ -2,10 +2,14 @@ from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QTex
     QTableWidgetItem, QLabel, QTabWidget, QApplication, QDesktopWidget, QMessageBox
 from PyQt5.QtGui import QFont, QBrush, QColor
 from PyQt5.QtCore import QDateTime, QTime, QTimer, Qt, QDate, pyqtSlot, QSize
-
+from datetime import date, datetime, timedelta
 import sys
 import pymysql
 import query
+import locale
+
+locale.setlocale(locale.LC_ALL, '')
+
 
 # import cv2
 # import glob
@@ -29,18 +33,22 @@ class MainWindow(QMainWindow):
         framePos.moveCenter(centerPos)
         self.move(framePos.topLeft())
 
-class Datetime(QDateTime):
+
+class Datetime1(QDateTime):
     def __init__(self, input_QDatetime=None):
         super().__init__()
-        self.setCurrenDateTime()
+        self.set_curren_datetime()
 
         if (self.date().year() % 4) == 0:
-            self._month_table = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] #윤년
-        else :
-            self._month_table = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] #평년
+            self._month_table = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  # 윤년
+        else:
+            self._month_table = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  # 평년
 
         if input_QDatetime != None:
             self.targetDateTime = input_QDatetime
+
+    def __str__(self):
+        return f'self.targetDateTime, QDateTime.toString(self.targetDateTime, "MM월 dd일 dddd  ap hh:mm")'
 
     def get_date(self):
         return [self.targetDateTime.date(), self.targetDateTime.date().toString("MM월 dd일 dddd")]
@@ -51,10 +59,10 @@ class Datetime(QDateTime):
     def get_dateTime(self):
         return [self.targetDateTime, QDateTime.toString(self.targetDateTime, "MM월 dd일 dddd  ap hh:mm")]
 
-    def set_currenDateTime(self):
+    def set_curren_datetime(self):
         self.targetDateTime = self.currentDateTime()
 
-    def set_completeDateTime(self):
+    def set_complete_datetime(self):
         self.targetDateTime = self.currentDateTime()
         self.targetDateTime.setTime(QTime(17, 0, 0, 0))
 
@@ -91,7 +99,7 @@ class Datetime(QDateTime):
                       input_compdate.time().minute(),
                       input_compdate.time().second()]
 
-        print(comp1_date,comp1_time, ":::",comp2_date,comp2_time)
+        print(comp1_date, comp1_time, ":::", comp2_date, comp2_time)
 
         buf_sublist = []  # [year, month, hour, minute, second]
         buf_sublist.append(comp2_date[0] - comp1_date[0])
@@ -104,19 +112,18 @@ class Datetime(QDateTime):
 
         rounding_table = [0, 0, 0, 24, 60]
 
-        print('@@@@@@@@',minus_index)
+        print('@@@@@@@@', minus_index)
 
         for i in minus_index:
             if i > 2:
                 buf_sublist[i - 1] -= 1
-                buf_sublist[i] += (rounding_table[i]+1)
+                buf_sublist[i] += (rounding_table[i] + 1)
 
-            elif i == 2 :
+            elif i == 2:
                 buf_sublist[i - 1] -= 1
-                buf_sublist[i] += (self._month_table[comp1_date[1]]+1)
+                buf_sublist[i] += (self._month_table[comp1_date[1]] + 1)
 
-
-        datetime_header = ['year','month','date','hour','minute']
+        datetime_header = ['year', 'month', 'date', 'hour', 'minute']
 
         result = {}
 
@@ -126,6 +133,45 @@ class Datetime(QDateTime):
         return result
 
 
+class Datetime(datetime):
+    def __new__(cls, *args):
+        return super(Datetime, cls).__new__(cls, *args)
+
+    def __init__(self, *args):
+        super().__init__()
+
+        if not self.year % 4:
+            _MONTH_TABLE = (31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)  # 윤년
+        else:
+            _MONTH_TABLE = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)  # 평년
+
+    def __str__(self):
+        return f"{self.strftime('%Y%m월 %d일(%A) %p %I시 %M분').encode().decode('UTF-8')}"
+
+    def __repr__(self):
+        return f"{self.strftime('%Y%m월 %d일(%A) %p %I시 %M분').encode().decode('UTF-8')}"
+
+    def get_datetime(self):
+        return (self.year,self.month,self.day, self.hour, self.second)
+
+    def compare_datetime(self,compare_datetime):
+        #(self.year, self.month, self.day, self.hour, self.second)
+        my_datetime = (self.year,
+                        self.month,
+                        self.day,
+                        self.hour,
+                        self.second)
+        compare_datetime = (compare_datetime.year,
+                            compare_datetime.month,
+                            compare_datetime.day,
+                            compare_datetime.hour,
+                            compare_datetime.second)
+
+        return ([i-j for i,j in zip(my_datetime,compare_datetime)])
+
+    def set_completetime(self):
+        self.time(1,1,1)
+
 
 if __name__ == '__main__':
     # app = QApplication(sys.argv)
@@ -133,17 +179,31 @@ if __name__ == '__main__':
     #
     # sys.exit(app.exec_())
 
-    dateTime = Datetime()
-    dateTime.set_currenDateTime()
-    complete = Datetime()
-    complete.setCompleteDateTime()
+    # dateTime = Datetime()
+    # dateTime.set_curren_datetime()
+    # complete = Datetime()
+    # complete.set_complete_datetime()
+    #
+    # compareDateTime = QDateTime()
+    # compareDateTime.setDate(QDate(2019, 8, 9))
+    # compareDateTime.setTime(QTime(14, 40, 0, 0))
+    #
+    # print('111111111111',compareDateTime)
+    #
+    # dateTime1 = Datetime(dateTime.get_dateTime()[0])
+    # sub_dateTime = dateTime1.compare_datetime(compareDateTime)
+    #
+    # for timeunit, value in sub_dateTime.items():
+    #     print(timeunit, value)
 
-    compareDateTime = QDateTime()
-    compareDateTime.setDate(QDate(2019, 8, 9))
-    compareDateTime.setTime(QTime(14, 40, 0, 0))
+    day1 = Datetime.today()
+    print(day1)
 
-    dateTime1 = Datetime(dateTime.get_dateTime()[0])
-    sub_dateTime = dateTime1.compare_datetime(compareDateTime)
+    day2 = Datetime.today()
+    plus_datetime = timedelta(hours=13)
+    day2 = day2 + plus_datetime
 
-    for timeunit, value in sub_dateTime.items():
-        print(timeunit, value)
+    print(day1.compare_datetime(day2))
+
+    print(day2.set_completetime())
+
